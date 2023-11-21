71e2dd70e50d80604b626ddb19b9cc1f7c6ebd8a
NachoGV <ignacio.gvalverde@gmail.com>
2023-11-21 17:31:02 -0600

    Transformer Results Upgrade

--- utils/plot_coco_image.py
+++ utils/plot_coco_image.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw   
 
-def plot_coco_image(coco_dataset, imgs_path, image_id):
+def plot_coco_image(coco_dataset, imgs_path, image_id, width):
 
     # Category ID to Label Name
     id2label = {k: v["name"] for k, v in coco_dataset.coco.cats.items()}
@@ -12,12 +12,12 @@ def plot_coco_image(coco_dataset, imgs_path, image_id):
 
     # Image
     image = coco_dataset.coco.loadImgs(image_id)[0]
-    print("Image ID: ", image_id, "Image: ", image["file_name"])
+    print("\nImage ID: ", image_id, "Image: ", image["file_name"])
 
     # Image Annotations
     annotation_ids = coco_dataset.coco.getAnnIds(imgIds=image_id)
     annotations = coco_dataset.coco.loadAnns(annotation_ids)
-    print("\nAnnotations: ")
+    print("\nGround Truth Annotations: ")
     for anno in annotations:
         print(anno, 'Category Label: ', id2label[anno['category_id']])
 
@@ -28,7 +28,6 @@ def plot_coco_image(coco_dataset, imgs_path, image_id):
     for anno in annotations:
         cat_id = anno['category_id']
         bbox = [int(b) for b in anno["bbox"]]
-        draw.rectangle([bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]], width=7, outline=colors[cat_id])
+        draw.rectangle([bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]], width=width, outline=colors[cat_id])
         draw.text([bbox[0], bbox[1]], id2label[cat_id], fill=colors[cat_id])
     plt.imshow(img)
-    plt.show()
