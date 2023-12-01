30e79edbbf7a944c3c96ee8408c85662ee6958f1
NachoGV <ignacio.gvalverde@gmail.com>
2023-12-01 16:01:49 -0600

    Transformer result final? version

--- README.md
+++ README.md
@@ -7,29 +7,22 @@ This project aims to compare and evaluate the performance of transformer-based a
 The Exclusively Dark [(ExDark)](https://github.com/cs-chan/Exclusively-Dark-Image-Dataset) dataset contains the largest collection of natural low-light images taken in visible light to date, including object level annotation. 
 ### Dataset Folders Structure
 In git's repository root folder:
-* [./ExDark/ExDark]()(#TODO - Link Missing) - Original images from ExDark's git repository, subfolers for categories
-* [./ExDark_Annno/ExDark_Annno]()(#TODO - Link Missing) - Original annotations from ExDark's git repository, subfolders for categories
-* [./ExDark_All]()(#TODO - Link Missing) - All images and annotations without subfolders
-* [./ExDark_COCO]()(#TODO - Link Missing) - .JSON files for COCO format dataset generator (used by DETR)
+* [./ExDark/ExDark](https://1drv.ms/f/s!AhUfkuJTmKszjIt7uUgt9Ea0R-gfMw?e=TE6bYi) - Original images from ExDark's git repository, subfolers for categories
+* [./ExDark_Annno/ExDark_Annno](https://1drv.ms/f/s!AhUfkuJTmKszjNUT0MVaKq3t99LkUA?e=Ks5Cr1) - Original annotations from ExDark's git repository, subfolders for categories
+* [./ExDark_All](https://1drv.ms/f/s!AhUfkuJTmKszjZBa7Ij9EjyUf6uNkw?e=UJ1bug) - All images and annotations without subfolders
+* [./ExDark_COCO](https://1drv.ms/f/s!AhUfkuJTmKszjc59AqGH3wR743MjQg?e=rf0Ve6) - .JSON files for COCO format dataset generator (used by DETR)
 * [./ExDark_YOLO]()(#TODO - Link Missing) - File for YOLO model training 
 ### Split
 * 3000 images for training - 250 per class
 * 1800 images for validation - 150 per class
 * 2563 images for testing - rest of the images per class
 
-## Documents
-* Project Intermediate Report - [Word Document](https://iit0-my.sharepoint.com/personal/pvidhyaravikumar_hawk_iit_edu/Documents/DL_Intermediate%20Project%20Report.docx?d=w016b9bc6dead47829f1876795bf3bb6e&csf=1&web=1&e=LKwkTV)
-* Project Presentation - [Google PPT Document](https://docs.google.com/presentation/d/1wyljypQYRHxmpP_kKwDI-fUVvFBJUGjmEKa7qZp6xbk/edit?usp=sharing)
-* Project Final Report - [Latex Document](https://www.overleaf.com/project/6564e7e932fcc755bd703a53/invite/token/6933b1873c4cea570750be3901f9d68176afb2c156e6d546?project_name=DL_Project%20Report&user_first_name=pvidhyaravikumar)
-
-## Trained Models
-* Transformer w/clahe enhancement - [Link](https://drive.google.com/drive/folders/1xQKRGRJYcWPuGMsd8wHyDqoNQgpwhidP?usp=sharing)
-
 ## Authors
 * Ignacio Gomez Valverde (A20552714)
 * Prashanth V.R. (A20531508)
 
-## References
+## References 
+More references can be found in the project's final report
 ### YOLO
 * [You Only Look Once: Unified, Real-Time Object Detection](https://arxiv.org/abs/1506.02640)
 ### Transformer
@@ -38,4 +31,7 @@ In git's repository root folder:
 * [Using Custom Datasets to train DETR for object detection](https://medium.com/@soumyajitdatta123/using-custom-datasets-to-train-detr-for-object-detection-75a6426b3f4e)
 
 ## Anexes
-* Google Drive with the *Dataset* folder structure and *Trained Models* - [Link](https://drive.google.com/drive/folders/1zpYonlrYSMD-3p6Hj6gTCJJwBtTl5TOV?usp=sharing)
+* [One Drive](https://1drv.ms/f/s!AhUfkuJTmKszjIo-J1vB4h9SwhbiAQ?e=D1NjSo) with the *Dataset* folder structure and *Trained Models*
+* Project Intermediate Report - [Word Document](https://iit0-my.sharepoint.com/personal/pvidhyaravikumar_hawk_iit_edu/Documents/DL_Intermediate%20Project%20Report.docx?d=w016b9bc6dead47829f1876795bf3bb6e&csf=1&web=1&e=LKwkTV)
+* Project Presentation - [Google PPT Document](https://docs.google.com/presentation/d/1wyljypQYRHxmpP_kKwDI-fUVvFBJUGjmEKa7qZp6xbk/edit?usp=sharing)
+* Project Final Report - [Latex Document](https://www.overleaf.com/project/6564e7e932fcc755bd703a53/invite/token/6933b1873c4cea570750be3901f9d68176afb2c156e6d546?project_name=DL_Project%20Report&user_first_name=pvidhyaravikumar)
--- utils/plots.py
+++ utils/plots.py
@@ -121,3 +121,95 @@ def plot_image_vs_prediction(image_id, test_path, model_path, width, enhancement
     plt.imshow(pred_image)
     plt.title("Prediction")
     plt.show()
+
+# --------------------------------------------------------------------------------
+
+def plot_image(image_id, test_path, width, enhancement):
+
+    # CONSTANTS
+    imgs_path = "../ExDark_All/Images"
+    image_processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'magenta', 'lime', 'teal', 'lavender']
+
+    # Dataset Load
+    test_set = CocoDetection(image_directory_path=imgs_path,  annotation_file_path=test_path, image_processor=image_processor, enhancement=enhancement)
+
+    # Original Image
+    og_image, og_anno = test_set[image_id]
+    og_labels = og_anno['class_labels']
+    og_boxes = og_anno['boxes']
+    og_size = og_anno['size']
+    cv_image = cv2.imread(os.path.join(imgs_path, test_set.coco.loadImgs(image_id)[0]['file_name']))
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+
+    # Plots - Preparation
+    og_image = T.ToPILImage()(og_image)
+    og_draw = ImageDraw.Draw(og_image)
+
+    # Plots - Original
+    for i,box in enumerate(og_boxes):
+        og_box = [float(b) for b in box]
+        og_box = [og_box[0]*og_size[1], og_box[1]*og_size[0], og_box[2]*og_size[1], og_box[3]*og_size[0]]
+        og_box = [og_box[0]-og_box[2]/2, og_box[1]-og_box[3]/2, og_box[0]+og_box[2]/2, og_box[1]+og_box[3]/2]
+        og_draw.rectangle(og_box, outline=colors[og_labels[i]], width=width)
+    plt.imshow(og_image)
+    plt.axis('off')
+    plt.title("Ground Truth")
+
+# --------------------------------------------------------------------------------
+    
+def plot_prediction(image_id, test_path, model_path, width, enhancement):
+
+    # Device
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # CONSTANTS
+    imgs_path = "../ExDark_All/Images"
+    image_processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'magenta', 'lime', 'teal', 'lavender']
+
+    # Dataset Load
+    test_set = CocoDetection(image_directory_path=imgs_path,  annotation_file_path=test_path, image_processor=image_processor, enhancement=enhancement)
+    id2label = {k: v["name"] for k, v in test_set.coco.cats.items()}
+
+    # Original Image
+    og_image, og_anno = test_set[image_id]
+    og_size = og_anno['size']
+    cv_image = cv2.imread(os.path.join(imgs_path, test_set.coco.loadImgs(image_id)[0]['file_name']))
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+
+    # Predicted Image
+    model = DetrForObjectDetection.from_pretrained(model_path).to(device)
+    with torch.no_grad():
+        inputs = image_processor(images=cv_image, return_tensors='pt').to(device)
+        outputs = model(**inputs)
+        results = image_processor.post_process_object_detection(
+            outputs=outputs, 
+            threshold=0.001, 
+            target_sizes=[og_size]
+        )[0]
+
+    # Predicted Annotations only if score is above 80% confidence
+    pred_labels = []
+    pred_boxes = []
+    for i in range(len(results["scores"])):
+        if results["scores"][i] >= 0.85:
+            score = round(float(results["scores"][i]), 5)
+            label = int(results['labels'][i])
+            box = results["boxes"][i]
+            pred_labels.append(label)
+            pred_boxes.append(box)
+
+    # Plots - Preparation
+    og_image = T.ToPILImage()(og_image)
+    pred_image = og_image.copy()
+    pred_draw = ImageDraw.Draw(pred_image)
+
+    # Plots - Predicted
+    for i,box in enumerate(pred_boxes):
+        pred_box = [float(b) for b in box]
+        pred_box = [pred_box[0], pred_box[1], pred_box[2], pred_box[3]]
+        pred_draw.rectangle(pred_box, outline=colors[pred_labels[i]], width=width)
+    plt.imshow(pred_image)
+    plt.axis('off')
+    plt.title(f"{enhancement} Prediction")
